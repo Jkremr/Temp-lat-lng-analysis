41eeb2f449edb5d7e5c241376a20dc93eb782376
Jkremr <jacobaddisonkramer@gmail.com>
2017-08-21 15:50:12 -0700

    Adding logic to prevent duplicated cities from being selected in sample.

--- weather_analysis.py
+++ weather_analysis.py
@@ -5,8 +5,8 @@ import random
 
 city = citipy.nearest_city(0, 0)
 
-latDim = {'min': -90, 'max': 90}
-lngDim = {'min': -180, 'max': 180}
+latDim = {'min': -90, 'max': -89.9}
+lngDim = {'min': -180, 'max': -179.9}
 
 latVals = np.arange(latDim['min'], latDim['max'], 0.1)
 lngVals = np.arange(lngDim['min'], lngDim['max'], 0.1)
@@ -19,8 +19,11 @@ while counter < 5:
 	randLat = random.choice(latVals)
 	randLng = random.choice(lngVals)
 	city = citipy.nearest_city(randLat, randLng)
+	if city.city_name + city.country_code in cities.uniqueName.tolist():
+		pass
+	else:
+		cities.loc[len(cities)] = [city.city_name, city.country_code, randLat, randLng, city.city_name + city.country_code]
 
-	cities.loc[len(cities)] = [city.city_name, city.country_code, randLat, randLng, city.city_name + city.country_code]
 	counter += 1
 
 print(cities)
